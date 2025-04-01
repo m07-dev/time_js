6e3b6033ff9c381c510e8c063c88d6ad0fee74ea
Mohamed <fofanaa.mohamed45@gmail.com>
2025-04-01 22:51:35 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 const card = document.querySelector('.card')
+const time_text = document.querySelector('.time')
 
 const h = document.querySelector('.hours')
 const m = document.querySelector('.minutes')
 const s = document.querySelector('.secondes')
 
-// animation card 
+// card animation 
 
 card.animate([
     {transform:'translateY(20px)', opacity:0},
@@ -13,19 +14,20 @@ card.animate([
     duration:700
 })
 
+time_text.animate([
+    {transform:'translateY(20px)', opacity:0},
+    {transform:'translateY(0px)', opacity:1},
+],{
+    duration:1000
+})
+
 // time set 
-
-
-
-/*console.log(date.getSeconds())
-console.log(date.getMinutes())
-console.log(date.getHours())*/
 
 const time = setInterval(()=>{
     const date = new Date();
-    h.innerHTML = date.getHours()
-    m.innerHTML = date.getMinutes()
-    s.innerHTML = date.getSeconds()
+    h.innerHTML = date.getHours() + "h"
+    m.innerHTML = date.getMinutes() + "m"
+    s.innerHTML = date.getSeconds() + "s"
 }
 ,1000)
 
